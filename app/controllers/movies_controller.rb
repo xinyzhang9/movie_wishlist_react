e3becafc43be96a6d8d2357864279decc4f5d48f
Xinyang <xinyzhang9@gmail.com>
2016-03-10 23:52:16 -0800

    update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,10 +3,15 @@ require 'net/http'
 require 'open-uri'
 class MoviesController < ApplicationController
   def show
-      movie = "https://api.themoviedb.org/3/movie/"+params[:id]+"?api_key=" + APP_ID
+      movie = "http://api.themoviedb.org/3/movie/"+params[:id]+"?api_key=" + APP_ID
       uri = movie
       data = open(uri)
       @movie = JSON.load(data)
+      trailer = 'http://api.themoviedb.org/3/movie/'+params[:id]+'/videos?api_key=' + APP_ID
+      data_trailer = open(trailer)
+      @movie_trailer = JSON.load(data_trailer)['results'][0]
+      images = 'http://api.themoviedb.org/3/movie/'+params[:id]+'/images?api_key=' + APP_ID
+      @movie_images = JSON.load(open(images))['backdrops']
       # render :json => JSON.load(data)
   end
 end
